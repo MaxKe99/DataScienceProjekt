f8c218cf5bf48e58048445a375d2dc716d131eee
Max Kettenhofen <53062006+MaxKe99@users.noreply.github.com>
2021-01-04 14:53:33 +0100

    Update article_extraction.py

--- article_extraction.py
+++ article_extraction.py
@@ -4,10 +4,6 @@ import os
 from Giveme5W1H.extractor.extractor import MasterExtractor
 from Giveme5W1H.extractor.tools.file.handler import Handler
 
-from geopy.geocoders import Nominatim
-
-geolocator = Nominatim(user_agent="GiveMe5W1H_analysis")
-
 # don`t forget to start up core_nlp_host
 # giveme5w1h-corenlp
 
