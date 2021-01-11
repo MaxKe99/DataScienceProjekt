ef9e8050f827864ee35856af82f619879e4b74f2
Max Kettenhofen <53062006+MaxKe99@users.noreply.github.com>
2021-01-11 23:07:29 +0100

    Update newsplease_example.py

--- newsplease_example.py
+++ newsplease_example.py
@@ -4,21 +4,22 @@ from Giveme5W1H.extractor.document import Document
 from Giveme5W1H.extractor.extractor import MasterExtractor
 
 extractor = MasterExtractor()
-articles = ['https://www.bbc.com/news/business-55390858',
-            'https://www.bbc.com/news/business-55384111',
-            'https://www.bbc.com/news/business-55359320',
-            'https://www.bbc.com/news/business-55344670',
-            'https://www.bbc.com/news/business-55357340',
-            'https://www.bbc.com/news/entertainment-arts-55393972',
-            'https://www.bbc.com/news/entertainment-arts-55392779',
-            'https://www.bbc.com/news/entertainment-arts-55358301',
-            'https://www.bbc.com/news/entertainment-arts-55346397',
-            'https://www.bbc.com/news/entertainment-arts-55336353']
+#articles = ['https://www.bbc.com/news/business-55390858',
+#            'https://www.bbc.com/news/business-55384111',
+#            'https://www.bbc.com/news/business-55359320',
+#            'https://www.bbc.com/news/business-55344670',
+#            'https://www.bbc.com/news/business-55357340',
+#            'https://www.bbc.com/news/entertainment-arts-55393972',
+#            'https://www.bbc.com/news/entertainment-arts-55392779',
+#            'https://www.bbc.com/news/entertainment-arts-55358301',
+#            'https://www.bbc.com/news/entertainment-arts-55346397',
+#            'https://www.bbc.com/news/entertainment-arts-55336353']
             
-questions = ['who', 'what', 'when']
+articles = ['https://www.bbc.com/news/business-55390858']
 
 def main():
 
+    questions = ['who', 'what', 'where']
     for url in articles:
         article = NewsPlease.from_url(url)
         doc = Document.from_newsplease(article)
