96199bf00127038a50a4a0f419e4d0c90633f3f2
Max Kettenhofen <53062006+MaxKe99@users.noreply.github.com>
2021-01-12 10:52:25 +0100

    Update newsplease_example.py

--- newsplease_example.py
+++ newsplease_example.py
@@ -4,45 +4,55 @@ from Giveme5W1H.extractor.document import Document
 from Giveme5W1H.extractor.extractor import MasterExtractor
 
 extractor = MasterExtractor()
-#articles = ['https://www.bbc.com/news/business-55390858',
-#            'https://www.bbc.com/news/business-55384111',
-#            'https://www.bbc.com/news/business-55359320',
-#            'https://www.bbc.com/news/business-55344670',
-#            'https://www.bbc.com/news/business-55357340',
-#            'https://www.bbc.com/news/entertainment-arts-55393972',
-#            'https://www.bbc.com/news/entertainment-arts-55392779',
-#            'https://www.bbc.com/news/entertainment-arts-55358301',
-#            'https://www.bbc.com/news/entertainment-arts-55346397',
-#            'https://www.bbc.com/news/entertainment-arts-55336353']
+articles = ['https://www.bbc.com/news/business-55390858',
+            'https://www.bbc.com/news/business-55384111',
+            'https://www.bbc.com/news/business-55359320',
+            'https://www.bbc.com/news/business-55344670',
+            'https://www.bbc.com/news/business-55357340',
+            'https://www.bbc.com/news/entertainment-arts-55393972',
+            'https://www.bbc.com/news/entertainment-arts-55392779',
+            'https://www.bbc.com/news/entertainment-arts-55358301',
+            'https://www.bbc.com/news/entertainment-arts-55346397',
+            'https://www.bbc.com/news/entertainment-arts-55336353',
+            'https://www.bbc.com/news/uk-politics-55422729',
+            'https://www.bbc.com/news/uk-politics-55410349',
+            'https://www.bbc.com/news/uk-politics-55414981',
+            'https://www.bbc.com/news/uk-scotland-scotland-politics-55396311',
+            'https://www.bbc.com/news/uk-wales-politics-55423293',
+            'https://www.bbc.com/sport/football/55382530',
+            'https://www.bbc.com/sport/tennis/55416994',
+            'https://www.bbc.com/sport/golf/55395828',
+            'https://www.bbc.com/sport/athletics/55257397',
+            'https://www.bbc.com/sport/cycling/55337877',
+            'https://www.bbc.com/news/technology-55438969',
+            'https://www.bbc.com/news/technology-55439190',
+            'https://www.bbc.com/news/technology-55403473',
+            'https://www.bbc.com/news/technology-55415350',
+            'https://www.bbc.com/news/technology-55426212']
             
-articles = ['https://www.bbc.com/news/business-55390858']
+
 
 def main():
 
-    questions = ['who', 'what', 'where']
+    questions = ['who', 'what', 'when', 'where']
     for url in articles:
         article = NewsPlease.from_url(url)
         doc = Document.from_newsplease(article)
         doc = extractor.parse(doc)
         answers = []
         print(url)
-        #for q in questions:
-        #    answers.append(doc.get_top_answer(q).get_parts_as_text())
+        try:
+            for q in questions:
+                answers.append(doc.get_top_answer(q).get_parts_as_text())
         
-        #for i in range(len(answers)):
-        #    print(answers[i])
+            for i in range(len(answers)):
+                print(answers[i])
         
-        top_who_answer = doc.get_top_answer('who').get_parts_as_text()
-        top_what_answer = doc.get_top_answer('what').get_parts_as_text()
-        top_where_answer = doc.get_top_answer('where').get_parts_as_text()
-
-
-        print(top_who_answer)
-        print(top_what_answer)
-        print(top_where_answer)
-
-        #print('\n')
-
+        except:
+            print("Giveme5W1H could not find the answers.")
+            
+        finally:
+            print('\n')
 
 if __name__ == '__main__':
     main()  
